9334c95a11124bd1ef0903e4e833c6e669e6e73d
mingkyyy <54256348+mingkyyy@users.noreply.github.com>
2021-06-13 11:33:06 +0900

    storeView

--- miniproject/src/Gui/StoreView.java
+++ miniproject/src/Gui/StoreView.java
@@ -1,7 +1,6 @@
 package Gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.TextArea;
@@ -15,25 +14,28 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 
 
-public class StoreView extends JFrame{
+public class StoreView extends JFrame implements ActionListener{
 	private JPanel bigpanel,leftPanel,rightlPanel,storePanel,buttonPanel;
 	private TextArea textarea;
 	private TextArea textarea2;
 	private JButton addbutton, backButton,deletebutton,updatebutton;
+	private JLabel storename;
 	private JTable table;
 	private JScrollPane scrollPane;
 	public String foodname, foodpicture,foodprice;
 	
 	
+
+
 	public StoreView(){	
 		super("미니 프로젝트");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(800, 800);
 		setLocationRelativeTo(null);
 		
+		storename=new JLabel("가게 이름");
 		
 		textarea=new TextArea();		
 		textarea.setPreferredSize(new Dimension(400,600));
@@ -53,51 +55,21 @@ public class StoreView extends JFrame{
 		
 		addbutton=new JButton("추가");
 		addbutton.setPreferredSize(new Dimension(150,100));
-        addbutton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String foodname=JOptionPane.showInputDialog("추가할 음식 이름을 입력하세요");
-				String foodprice=JOptionPane.showInputDialog("추가할 음식 가격을 입력하세요");
-				String foodpicture=JOptionPane.showInputDialog("추가할 사진 경로를 입력하세요");
-				
-			    DefaultTableModel m=(DefaultTableModel)table.getModel();
-			    m.insertRow(0, new Object[] {foodname,foodprice,foodpicture});
-				table.updateUI();
-				
-				
-			}
-		});
+        addbutton.addActionListener(this);
         
         deletebutton=new JButton("삭제");
         deletebutton.setPreferredSize(new Dimension(150,100));
-        deletebutton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				
-				
-			}
-		});
+        deletebutton.addActionListener(this);
+        
         
         updatebutton=new JButton("수정");
         updatebutton.setPreferredSize(new Dimension(150,100));
+        updatebutton.addActionListener(this);
+        
 		
 		backButton=new JButton("back");
 		backButton.setPreferredSize(new Dimension(60,5));
-		backButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MainView();
-				setVisible(false);
-				
-			}
-		});
-		
-
-
+		backButton.addActionListener(this);
 		
 		
 		buttonPanel=new JPanel();
@@ -111,6 +83,7 @@ public class StoreView extends JFrame{
 		storePanel=new JPanel();
 		storePanel.setLayout(new BorderLayout());
 		storePanel.add(backButton,BorderLayout.WEST);
+		storePanel.add(storename,BorderLayout.CENTER);
 		
 		
 		storePanel.setPreferredSize(new Dimension(500,30));
@@ -147,4 +120,69 @@ public class StoreView extends JFrame{
 		
 	}
 
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(addbutton)) {
+			String foodname=JOptionPane.showInputDialog("추가할 음식 이름을 입력하세요");
+			String foodprice=JOptionPane.showInputDialog("추가할 음식 가격을 입력하세요");
+			String foodpicture=JOptionPane.showInputDialog("추가할 사진 경로를 입력하세요");
+			
+		    DefaultTableModel m=(DefaultTableModel)table.getModel();
+		    m.insertRow(0, new Object[] {foodname,foodprice,foodpicture});
+			table.updateUI();
+			
+		}
+		else if(e.getSource().equals(deletebutton)) {
+			String deletefood=JOptionPane.showInputDialog("삭제 할 음식이름을 입력하세요");
+			
+			for(int i=0; i<table.getRowCount(); i++) {
+				if(deletefood.equals(table.getValueAt(i, 0))) {
+					DefaultTableModel m=(DefaultTableModel)table.getModel();
+					m.removeRow(i);
+					return;
+				}else {
+					JOptionPane.showMessageDialog(null, "삭제할 음식이 존재 하지 않습니다");
+					return;
+				}
+			}
+			
+		}else if(e.getSource().equals(updatebutton)) {
+			String updatefood=JOptionPane.showInputDialog("수정할 음식 이름을 선택하세요");
+			String update=JOptionPane.showInputDialog("1:이름수정, 2: 가격 수정, 3:사진 수정");
+			
+			for(int i=0; i<table.getRowCount(); i++) {
+				if(updatefood.equals(table.getValueAt(i, 0))) {		
+					if(update.equals("1")) {
+						DefaultTableModel m=(DefaultTableModel)table.getModel();
+						String newname = JOptionPane.showInputDialog("수정 할 이름을 입력하세요");						
+						m.setValueAt(newname, i, 0);
+						return;
+						
+					}else if(update.equals("2")) {
+						String newprice = JOptionPane.showInputDialog("수정 할 가격 입력하세요");						
+						DefaultTableModel m=(DefaultTableModel)table.getModel();
+						m.setValueAt(newprice, i, 1);
+						return;
+					}else if(update.equals("3")) {
+						String newpicture = JOptionPane.showInputDialog("수정 할 사진 입력하세요");
+						DefaultTableModel m=(DefaultTableModel)table.getModel();
+						m.setValueAt(newpicture, i, 2);
+						return;
+					}
+				}else {
+					JOptionPane.showMessageDialog(null, "수정할 음식이 존재 하지 않습니다");
+					return;
+				}
+			
+			}
+		}else if(e.getSource().equals(backButton)) {
+			new MainView();
+			setVisible(false);
+			
+		}
+			
+		}
+		
+	}
+
+
